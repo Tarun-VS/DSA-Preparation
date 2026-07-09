8d4f4303ea6e93574cd6296e96812fda4897cc13
Tarun-VS <tarunvirsingh8@gmail.com>
2026-07-09 14:25:32 +0530

    Create LC2469_Convert_The_Temperature.cpp

---
+++ Math/LC2469_Convert_The_Temperature.cpp
@@ -0,0 +1,20 @@
+/*
+LeetCode 2469 - Convert the Temperature
+
+Topic:
+Math
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+
+        double kelvin = celsius + 273.15;
+        double fahrenheit = (celsius * 1.80) + 32.00;
+
+        return {kelvin, fahrenheit};
+    }
+};
