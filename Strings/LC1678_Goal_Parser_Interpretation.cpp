17262df046615113986ca933681b85aaf83363bd
Tarun-VS <tarunvirsingh8@gmail.com>
2026-07-16 21:39:53 +0530

    Create LC1678_Goal_Parser_Interpretation.cpp

---
+++ Strings/LC1678_Goal_Parser_Interpretation.cpp
@@ -0,0 +1,35 @@
+/*
+LeetCode 1678 - Goal Parser Interpretation
+
+Topic:
+Strings
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+class Solution {
+public:
+    string interpret(string command) {
+        string result = "";
+
+        for (int i = 0; i < command.size(); i++) {
+
+            if (command[i] == 'G') {
+                result += 'G';
+            }
+
+            else if (command[i] == '(' && command[i + 1] == ')') {
+                result += 'o';
+                i++;
+            }
+
+            else {
+                result += "al";
+                i += 3;
+            }
+        }
+
+        return result;
+    }
+};
